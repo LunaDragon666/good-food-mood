0b94f649313c9923eef2f1eea083d20c1545421b
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-07-16 14:00:47 +0200

    Update message.js

--- js/components/message.js
+++ js/components/message.js
@@ -1,5 +1,5 @@
 function theError(message = "Error") {
     return `<div class="error-message">
-                <i class="fas fa-exclamation-circle"></i> Uh-oh! Failed to upload results...
+                <i class="fas fa-exclamation-circle"></i> Failed to upload results ...
             </div>`;
  }
